4664248b9b8ea6659a2b85a1ade83a4d80fdb6fd
saurabhbarse <saurabhbarse@IN-SAURABHBARSE>
2023-02-01 00:57:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KPMG.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // TASK 1 Only .NET 
+            Console.WriteLine("This project is only for .NET");
+        }
+    }
+}
